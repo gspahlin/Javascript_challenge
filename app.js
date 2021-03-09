73b7655e5cf5f06026b4d702c4b30a368ef24db6
gspahlin <gspahlin@gmail.com>
2021-03-09 10:15:34 -0500

    table update called by a function

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ let tableData = data;
 //get a reference to the table body
 let tbody = d3.select("tbody");
 
-//loop through the data and log it
-data.forEach(function (ufoSighting) {
+//function to loop through the data and log it
+function t_update() { tableData.forEach(function (ufoSighting) {
     console.log(ufoSighting);
 
     //add a table row for each sighting that just got logged
@@ -22,33 +22,42 @@ data.forEach(function (ufoSighting) {
             
     });
 });
+}
+
+t_update()
+
+console.log(typeof t_update)
 
 // apply a filter to the date and tie it to an event listener
 
-let btn = d3.select("#filter-btn")
+//let btn = d3.select("#filter-btn")
 
-let input_date = d3.select(".form-control")
+//let input_date = d3.select(".form-control")
 
-let f_data = (function dateFilter(input_date){
-    return data.datetime < input_date; 
+//let f_data = (function dateFilter(input_date){
+//    return data.datetime < input_date; 
     
-});
+//});
 
-btn.on("click", f_data.forEach(function (ufoSighting){
-    console.log(ufoSighting);
+//console.log(typeof f_data)
+
+//console.log(typeof data)
+
+//btn.on("click", f_data.forEach(function (ufoSighting){
+//    console.log(ufoSighting);
 
     //add a table row for each sighting that just got logged
-    let row = tbody.append("tr");
+//    let row = tbody.append("tr");
 
     //log the key value pairs and append the values to td elements in the table
-    Object.entries(ufoSighting).forEach(function([key, value]){
-        console.log(key, value);
+//    Object.entries(ufoSighting).forEach(function([key, value]){
+//        console.log(key, value);
 
-        let cell = row.append("td");
-        cell.text(value)
+//        let cell = row.append("td");
+//        cell.text(value)
             
-    });
+//    });
 
-}));
+//}));
 
 
